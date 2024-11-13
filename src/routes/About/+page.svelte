b51d9fa263ac63c9644af9809805ae177c312ceb
Viswa B <theflipper070@gmail.com>
2024-11-14 01:56:22 +0530

    margin patch
    margin patch

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-[80vw] h-[80vh] place-self-center leading-normal justify-center">
+<div class="flex flex-col w-[80vw] h-[80vh] place-self-center leading-normal justify-center items-center">
     <span class = "font-semibold text-orange-500">Chennai Based Graphic Designer</span> with knowledge in Python, UI UX, 3d modelling, Game Development and animations
     Various interests come from various experiences in the above mentioned fields. <span><span class = "font-semibold text-orange-500">+4 Years experience</span> in the field of graphic designing
     Fresher in the field of UI/UX designing</span>
