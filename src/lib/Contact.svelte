23bfc2a85d9c5ec96ad25f5082dd1057f03741ea
Viswa B <theflipper070@gmail.com>
2024-12-20 19:41:28 +0530

    Update Contact.svelte

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,7 +1,7 @@
 <div class = "flex gap-2 absolute bottom-2 left-2">
-    <a href="mailto:viswab2002@gmail.com"><img src = "/gmail.png" alt = "gmail" class = "h-4"></a>
-    <a href = "https://www.instagram.com/whiz_whoa.img/"><img src = "/instagram.png" alt = "instagram" class = "h-4"></a>
-    <a href="https://www.linkedin.com/in/viswababu"><img src = "/linkedin.png" alt = "linkedin" class = "h-4"></a>
+    <a href="mailto:viswab2002@gmail.com" target = "_blank"><img src = "/gmail.png" alt = "gmail" class = "h-4"></a>
+    <a href = "https://www.instagram.com/whiz_whoa.img/" target = "_blank"><img src = "/instagram.png" alt = "instagram" class = "h-4"></a>
+    <a href="https://www.linkedin.com/in/viswababu" target = "_blank"><img src = "/linkedin.png" alt = "linkedin" class = "h-4"></a>
 </div>
 <div class = "w-full border-t border-t-black border-dashed h-8 absolute bottom-0 -z-10">
 </div>
